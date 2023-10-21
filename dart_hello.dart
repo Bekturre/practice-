2c0b41a85fb2ba5878bb59dbe0ce219abd1aa684
bekturre <bekturraimjanov1@gmail.com>
2023-10-21 20:25:34 +0600

    wd

--- dart_hello.dart
+++ dart_hello.dart
@@ -1,74 +1,98 @@
+
 import 'dart:io';
 
-void main() {
-  // 1. Сумма чисел в заданном диапазоне
-  stdout.write("Введите начало диапазона: ");
-  int start = int.parse(stdin.readLineSync()!);
-  stdout.write("Введите конец диапазона: ");
-  int end = int.parse(stdin.readLineSync()!);
-  int sum = 0;
+void main(){
+photofunc();
+}
 
-  for (int i = start; i <= end; i++) {
-    sum += i;
+
+nalogfunc(){
+print("Введите сумму дохода: ");
+  var doxod = double.tryParse(stdin.readLineSync()!) ?? 0;
+
+  double taxPercentage;
+  switch (doxod) {
+    case double.infinity:
+      taxPercentage = 0.12;
+     break;
+    default:
+    if (doxod <= 10000) {
+        taxPercentage = 0;
+      } else if (doxod <= 50000) {
+        taxPercentage = 0.1;
+      } else {
+        taxPercentage = 0.12;
+        
+      }
   }
-  print("Сумма чисел в заданном диапазоне: $sum");
 
-  // 2. Проверка на високосный год
-  stdout.write("Введите год для проверки на високосность: ");
-  int year = int.parse(stdin.readLineSync()!);
+  var tax = doxod * taxPercentage;
+  print("Сумма налога на доход: $tax");
+}
 
-  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-    print("$year - високосный год");
-  } else {
-    print("$year - не високосный год");
-  }
 
-  // 3. Наименьшее общее кратное
-  stdout.write("Введите первое число: ");
-  int num1 = int.parse(stdin.readLineSync()!);
-  stdout.write("Введите второе число: ");
-  int num2 = int.parse(stdin.readLineSync()!);
 
-  int max = (num1 > num2) ? num1 : num2;
-  int lcm;
 
-  while (true) {
-    if (max % num1 == 0 && max % num2 == 0) {
-      lcm = max;
+
+calcfunc(){
+ print("Введите первое число: ");
+  var num1 = int.tryParse(stdin.readLineSync()!) ?? 0;
+  
+  print("Введите (+, -, *, /): ");
+  var znak = stdin.readLineSync()!;
+  
+  print("Введите второе число: ");
+  var num2 = double
+  .tryParse(stdin.readLineSync()!) ?? 0;
+
+  double result;
+  switch (znak) {
+    case '+':
+      result = num1 + num2;
       break;
-    }
-    max++;
-  }
-  print("Наименьшее общее кратное: $lcm");
-
-  // 4. Сумма цифр числа
-  stdout.write("Введите целое число: ");
-  int inputNumber = int.parse(stdin.readLineSync()!);
-  int digitSum = 0;
-  int number = inputNumber.abs();
-
-  while (number > 0) {
-    digitSum += number % 10;
-    number ~/= 10;
-  }
-  print("Сумма цифр числа $inputNumber: $digitSum");
-
-  // 5. Проверка на палиндром
-  stdout.write("Введите слово или фразу: ");
-  String inputString = stdin.readLineSync()!.toLowerCase();
-  bool isPalindrome = true;
-
-  for (int i = 0, j = inputString.length - 1; i < j; i++, j--) {
-    if (inputString[i] != inputString[j]) {
-      isPalindrome = false;
+    case '-':
+      result = num1 - num2;
       break;
-    }
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+      } else {
+        print("Ошибка: деление на ноль.");
+        return;
+      }
+      break;
+    default:
+      return;
   }
+  print("Результат: $result");
+}
 
-  if (isPalindrome) {
-    print("$inputString - это палиндром");
-  } else {
-    print("$inputString - это не палиндром");
+
+
+
+
+void photofunc() {
+  print("Введите расширение файла  '.txt', '.jpg', '.pdf': ");
+  var choice = stdin.readLineSync()!.toLowerCase();
+
+  String fileType;
+  switch (choice) {
+    case '.txt':
+      fileType = "Текстовый файл";
+      break;
+    case '.jpg':
+     fileType = "Изображение";
+      break;
+    case '.pdf':
+      fileType = "Документ";
+      break;
+    default:
+      fileType = "Неизвестный тип файла";
+      break;
   }
+  print("Тип файла: $fileType");
 }
 
